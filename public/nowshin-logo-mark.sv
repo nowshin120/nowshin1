095d64fc3b606fed4192880c02caf3e33f798389
nowshin120 <amdadulhuq88@gmail.com>
2026-04-13 13:42:58 +0600

    Create nowshin-logo-mark.sv

---
+++ public/nowshin-logo-mark.sv
@@ -0,0 +1,25 @@
+<svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="512" height="512" rx="132" fill="url(#bg)" />
+  <rect x="48" y="48" width="416" height="416" rx="112" stroke="url(#frame)" stroke-width="10" />
+  <path
+    d="M158 366V146H210L304 284V146H354V366H303L208 226V366H158Z"
+    fill="url(#letter)"
+  />
+  <defs>
+    <linearGradient id="bg" x1="56" y1="50" x2="448" y2="462" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#08101F" />
+      <stop offset="0.6" stop-color="#10192D" />
+      <stop offset="1" stop-color="#172554" />
+    </linearGradient>
+    <linearGradient id="frame" x1="77" y1="74" x2="434" y2="440" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#F7E0A3" />
+      <stop offset="0.5" stop-color="#C69444" />
+      <stop offset="1" stop-color="#F5E6BC" />
+    </linearGradient>
+    <linearGradient id="letter" x1="172" y1="146" x2="348" y2="366" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#FFF8E8" />
+      <stop offset="0.52" stop-color="#F3D58A" />
+      <stop offset="1" stop-color="#D7A64D" />
+    </linearGradient>
+  </defs>
+</svg>
